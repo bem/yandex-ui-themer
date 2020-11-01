0186bf0d7dbd2d1ea6de64171f57baf51cdb26d7
AnyaTyu <anya.tyutyunnik@yandex.ru>
2020-11-01 15:53:30 +0300

    chore: init repo

---
+++ themekit.config.js
@@ -0,0 +1,95 @@
+const { Api } = require('@yandex/themekit');
+
+const componentsList = [
+  "attach",
+  "badge",
+  "button",
+  "checkbox",
+  "divider",
+  "icon",
+  "image",
+  "layerManager",
+  "link",
+  "listTile",
+  "menu",
+  "messageBox",
+  "modal",
+  "popup",
+  "progress",
+  "radioButton",
+  "radiobox",
+  "select",
+  "showcase",
+  "spacer",
+  "spin",
+  "tabsMenu",
+  "tabsPanes",
+  "text",
+  "textarea",
+  "textinput",
+  "theme",
+  "tooltip",
+  "tumbler",
+  "userPic",
+]
+
+Api.registerFormat({
+  name: 'json/extended',
+  formatter(dict) {
+    const globals = {}
+    const components = {};
+
+    for (const prop of dict.allProperties) {
+      if (componentsList.includes(prop.path[0])) {
+        if (components[prop.path[0]] === undefined) {
+          components[prop.path[0]] = {}
+        }
+        components[prop.path[0]] = {
+          ...components[prop.path[0]],
+          [prop.name]: {
+            value: prop.value,
+            path: prop.path,
+            description: prop.comment,
+          }
+        }
+      } else {
+        globals[prop.name] = {
+          value: prop.value,
+          path: prop.path,
+          description: prop.comment,
+        }
+      }
+    }
+
+    return JSON.stringify({
+      globals,
+      components,
+    }, null, 2)
+  },
+})
+
+module.exports = {
+  "entry": {
+    "example": "./src/themes/example.theme.json"
+  },
+  "output": {
+    "css": {
+      "transforms": ["name/cti/kebab", "name/mapper"],
+      "buildPath": "./src/themes/presets",
+      "actions": ["process-color"],
+      "files": [
+        {
+          "destination": "[entry]/root.css",
+          "format": "css/variables",
+          "options": {
+            "selector": ".Theme_root_[entry]"
+          }
+        },
+        {
+          "destination": "[entry]/root.json",
+          "format": "json/extended"
+        }
+      ]
+    }
+  }
+}
