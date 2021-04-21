50a8c86400aa287855439d7b43c6d236974100cb
Nikita Nigmatullin <nnigmat@gmail.com>
2021-04-21 17:31:44 +0300

    feat: add LinkGenerator and loading tokens on page load

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import { theme } from '@yandex/ui/Theme/presets/default'
 import { Select } from '@yandex/ui/Select/desktop/bundle'
 import { ListTile } from '@yandex/ui/ListTile/desktop'
 import { Text } from '@yandex/ui/Text/bundle'
+import { useSearchParam } from 'react-use'
+
+import { theme as themeDefault } from '@yandex/ui/Theme/presets/default'
+import { theme as themeInverse } from '@yandex/ui/Theme/presets/inverse'
+import { theme as themeBrand } from '@yandex/ui/Theme/presets/brand'
 
 import stylesDataDefault from './themes/presets/example/root.json'
 import stylesDataInverse from './themes/presets/example-inverse/root.json'
@@ -14,57 +19,65 @@ import mappingsDataInverse from './themes/presets/example-inverse/mappings.json'
 import mappingsDataBrand from './themes/presets/example-brand/mappings.json'
 
 import { Sandbox } from './components/Sandbox/Sandbox'
+import { LinkGenerator } from './components/LinkGenerator'
 
-import { theme as themeDefault} from '@yandex/ui/Theme/presets/default'
-import { theme as themeInverse} from '@yandex/ui/Theme/presets/inverse'
-import { theme as themeBrand} from '@yandex/ui/Theme/presets/brand'
+import { ThemeType } from './types'
 
-import { ThemeType } from './types';
+import { useTokensDownloader } from './utils/useTokensDownloader'
 
 import './App.css'
+
 configureRootTheme({ theme })
 
-
 const themes: Record<string, ThemeType> = {
-    default: { tokens: stylesDataDefault, mappings: mappingsDataDefault, preset: themeDefault },
-    inverse: { tokens: stylesDataInverse, mappings: mappingsDataInverse, preset: themeInverse },
-    brand: { tokens: stylesDataBrand, mappings: mappingsDataBrand, preset: themeBrand },
+  default: { tokens: stylesDataDefault, mappings: mappingsDataDefault, preset: themeDefault },
+  inverse: { tokens: stylesDataInverse, mappings: mappingsDataInverse, preset: themeInverse },
+  brand: { tokens: stylesDataBrand, mappings: mappingsDataBrand, preset: themeBrand },
 }
 
 export default () => {
-    const themesNames = Object.keys(themes);
-    const [theme, chooseTheme] = useState('default')
+  const themesNames = Object.keys(themes)
+  const [theme, chooseTheme] = useState('default')
+  const tokensHash = useSearchParam('tokensHash')
 
-    const menuItems = themesNames.map(val => ({
-        value: val,
-        content: themes[val].name || `${val}`,
-    }))
+  useTokensDownloader(tokensHash || '')
 
-    return (
-        <div className="Site">
-            <div className="Header">
-                <ListTile
-                    leftSpace="m"
-                    rightSpace="m"
-                    alignItems="center"
-                    leading={
-                        <Text typography="control-m" color="secondary">
-                            Тема из пресетов @yandex/ui:
-                        </Text>
-                    }
-                >
-                    <Select
-                        size="m"
-                        view="default"
-                        options={menuItems}
-                        value={theme}
-                        onChange={(event: any) => chooseTheme(event.target.value)}
-                    />
-                </ListTile>
-            </div>
-            <div className="Content">
-                <Sandbox theme={themes[theme].preset} globals={themes[theme].tokens.globals} components={themes[theme].tokens.components} mappings={themes[theme].mappings} />
-            </div>
-        </div>
-    )
+  const menuItems = themesNames.map((val) => ({
+    value: val,
+    content: themes[val].name || `${val}`,
+  }))
+
+  return (
+    <div className="Site">
+      <div className="Header">
+        <ListTile
+          leftSpace="m"
+          rightSpace="m"
+          alignItems="center"
+          leading={
+            <Text typography="control-m" color="secondary">
+              Тема из пресетов @yandex/ui:
+            </Text>
+          }
+        >
+          <Select
+            size="m"
+            view="default"
+            options={menuItems}
+            value={theme}
+            onChange={(event: any) => chooseTheme(event.target.value)}
+          />
+          <LinkGenerator />
+        </ListTile>
+      </div>
+      <div className="Content">
+        <Sandbox
+          theme={themes[theme].preset}
+          globals={themes[theme].tokens.globals}
+          components={themes[theme].tokens.components}
+          mappings={themes[theme].mappings}
+        />
+      </div>
+    </div>
+  )
 }
