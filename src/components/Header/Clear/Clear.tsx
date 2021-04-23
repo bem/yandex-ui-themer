a624272355c7bb4ac09ee52341a25436949bce52
Nikita Nigmatullin <nnigmat@gmail.com>
2021-04-23 12:37:52 +0300

    feat: добавил кнопку Сбросить тему

---
+++ src/components/Header/Clear/Clear.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+import { Button, IButtonProps as ButtonProps } from '@yandex/ui/Button/desktop/bundle'
+
+import { resetEvent } from '../../../state/tokens'
+
+export const Clear: FC<ButtonProps> = (props) => {
+  return (
+    <Button {...props} view="clear" size="m" onClick={resetEvent}>
+      Сбросить
+    </Button>
+  )
+}
