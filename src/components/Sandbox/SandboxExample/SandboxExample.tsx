6578ba0346088b2b9171a4495065003c9c7010f8
Nikita Nigmatullin <nnigmat@gmail.com>
2021-05-12 17:08:19 +0300

    chore: moved css and yaml text generation to model

--- src/components/Sandbox/SandboxExample/SandboxExample.tsx
+++ src/components/Sandbox/SandboxExample/SandboxExample.tsx
@@ -1,63 +1,46 @@
-import React, { FC, useState, useEffect } from 'react'
+import React, { FC, useState } from 'react'
 import { useStore } from 'effector-react'
-import deepmerge from 'deepmerge'
-import YAML from 'yaml'
 import { cnTheme } from '@yandex/ui/Theme'
 
 import { TabsMenu } from '@yandex/ui/TabsMenu/TabsMenu.bundle/desktop'
 import { Tumbler } from '@yandex/ui/Tumbler/Tumbler.bundle/desktop'
 
-import { toDeepToken } from '../../../utils/toDeepToken'
 import { Showcase } from '../../Showcase'
 import { metricaGoal } from '../../YaMetrika'
 
-import { $cssVariables, $designTokens } from '../../../model/tokens'
+import { $cssVariables, $cssText, $yamlText } from '../../../model/tokens'
+
+const JSX = 'JSX'
+const YAML = 'YAML'
+const CSS = 'CSS'
+
+const tabsNames = [JSX, YAML, CSS]
 
 export const SandboxExample: FC<any> = ({ includes, theme }) => {
-  const [activeTab, setActiveTab] = useState('jsx')
+  const [activeTab, setActiveTab] = useState(JSX)
 
   const cssVariables: Record<string, any> = useStore($cssVariables)
-  const designTokens = useStore($designTokens)
-
-  const [yml, setYml] = useState('')
-  const [css, setCss] = useState('')
+  const cssText = useStore($cssText)
+  const yamlText = useStore($yamlText)
   const [shownDiff, setDiff] = useState(true)
 
-  useEffect(() => {
-    const createCSS = () => {
-      const a = Object.keys(cssVariables).reduce((acc: string, v: string) => {
-        if (designTokens[v.replace('--', '')].changed) {
-          acc += `  ${v}: ${cssVariables[v]};\n`
-        }
-        return acc
-      }, '')
-      const r = `:root {\n${a}}`
+  const handleTabChange = (tab: string) => {
+    setActiveTab(tab)
+    metricaGoal(tab)
+  }
 
-      setCss(r)
-    }
+  const handleDiffChange = () => {
+    setDiff((prev) => !prev)
+    metricaGoal('show-hide-changes')
+  }
 
-    const createYaml = () => {
-      const yml = Object.entries(designTokens).reduce((acc, value: any) => {
-        if (value[1].changed) {
-          acc.push(toDeepToken(value[1].path, { value: value[1].value }))
-        }
-        return acc
-      }, [] as any)
-
-      const c = deepmerge.all(yml)
-      const text = YAML.stringify(c)
-
-      setYml(text)
-    }
-
-    if (activeTab === 'yml') {
-      createYaml()
-    }
-
-    if (activeTab === 'css') {
-      createCSS()
-    }
-  }, [activeTab, designTokens, cssVariables])
+  const tabs = tabsNames.map((tab) => ({
+    id: tab,
+    onClick: () => {
+      handleTabChange(tab)
+    },
+    content: tab,
+  }))
 
   return (
     <div className="Sandbox-Examples">
@@ -65,47 +48,13 @@ export const SandboxExample: FC<any> = ({ includes, theme }) => {
         <div className="Sandbox-Showcase">
           <div className="Sandbox-Content">
             <div className="Sandbox-Tabs">
-              <TabsMenu
-                size="m"
-                view="default"
-                layout="horiz"
-                activeTab={activeTab}
-                tabs={[
-                  {
-                    id: 'jsx',
-                    onClick: () => {
-                      setActiveTab('jsx')
-                      metricaGoal('jsx')
-                    },
-                    content: 'JSX',
-                  },
-                  {
-                    id: 'yml',
-                    onClick: () => {
-                      setActiveTab('yml')
-                      metricaGoal('yml')
-                    },
-                    content: 'YML',
-                  },
-                  {
-                    id: 'css',
-                    onClick: () => {
-                      setActiveTab('css')
-                      metricaGoal('css')
-                    },
-                    content: 'CSS',
-                  },
-                ]}
-              />
+              <TabsMenu size="m" view="default" layout="horiz" activeTab={activeTab} tabs={tabs} />
               <div className="Tumbler-Wrapper">
                 <Tumbler
                   size="m"
                   view="default"
                   checked={shownDiff}
-                  onChange={() => {
-                    setDiff(!shownDiff)
-                    metricaGoal('show-hide-changes')
-                  }}
+                  onChange={handleDiffChange}
                   labelAfter={'Показать изменения'}
                 />
               </div>
@@ -114,18 +63,18 @@ export const SandboxExample: FC<any> = ({ includes, theme }) => {
               className={cnTheme(theme, ['Sandbox-ContentWrapper'])}
               style={shownDiff ? cssVariables : {}}
             >
-              {activeTab === 'jsx' && <Showcase includes={includes} />}
-              {activeTab === 'yml' && (
+              {activeTab === JSX && <Showcase includes={includes} />}
+              {activeTab === CSS && (
                 <div className="Sandbox-ContentWrapper-YML">
                   <pre>
-                    <code>{yml}</code>
+                    <code>{cssText}</code>
                   </pre>
                 </div>
               )}
-              {activeTab === 'css' && (
+              {activeTab === YAML && (
                 <div className="Sandbox-ContentWrapper-YML">
                   <pre>
-                    <code>{css}</code>
+                    <code>{yamlText}</code>
                   </pre>
                 </div>
               )}
