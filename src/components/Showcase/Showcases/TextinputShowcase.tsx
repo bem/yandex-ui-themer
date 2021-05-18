d62aa58bb43325614f952c2d60daac0144570f38
Nikita Nigmatullin <nnigmat@gmail.com>
2021-05-18 16:57:41 +0300

    chore: добавил примеры для Textinput, Textarea с state=error

--- src/components/Showcase/Showcases/TextinputShowcase.tsx
+++ src/components/Showcase/Showcases/TextinputShowcase.tsx
@@ -1,18 +1,20 @@
-import React, { useState } from 'react';
-import { Textinput } from '@yandex/ui/Textinput/desktop/bundle';
+import React, { useState } from 'react'
+import { Textinput } from '@yandex/ui/Textinput/desktop/bundle'
 
-import { Headline } from '../../Headline/Headline';
+import { Headline } from '../../Headline/Headline'
 
 export const TextinputShowcase = () => {
   const [value1, setValue1] = useState('')
   const [value2, setValue2] = useState('')
   const [value3, setValue3] = useState('')
   const [value4, setValue4] = useState('')
+  const [value5, setValue5] = useState('')
+  const [value6, setValue6] = useState('')
 
   return (
     <>
       <Headline>Textinput</Headline>
-      <div className="Showcase-Item">
+      <div className="Showcase-Item" style={{ whiteSpace: 'normal' }}>
         <div style={{ padding: 4 }}>
           <Textinput
             hasClear
@@ -33,30 +35,53 @@ export const TextinputShowcase = () => {
             onClearClick={() => setValue2('')}
           />
         </div>
-      </div>
-      <div className="Showcase-Item">
         <div style={{ padding: 4 }}>
           <Textinput
             hasClear
             size="s"
-            label="Label"
-            view="material"
+            view="default"
             value={value3}
             onChange={(event) => setValue3(event.target.value)}
-            onClearClick={() => setValue2('')}
+            onClearClick={() => setValue3('')}
+            state="error"
+            hint="Error message"
           />
         </div>
-        <div style={{ padding: 4 }}>
-          <Textinput
-            hasClear
-            label="Label"
-            size="m"
-            view="material"
-            value={value4}
-            onChange={(event) => setValue4(event.target.value)}
-            onClearClick={() => setValue2('')}
-          />
-        </div>
+      </div>
+      <div style={{ padding: 4 }}>
+        <Textinput
+          hasClear
+          size="s"
+          label="Label"
+          view="material"
+          value={value4}
+          onChange={(event) => setValue4(event.target.value)}
+          onClearClick={() => setValue4('')}
+        />
+      </div>
+      <div style={{ padding: 4 }}>
+        <Textinput
+          hasClear
+          label="Label"
+          size="m"
+          view="material"
+          value={value5}
+          onChange={(event) => setValue5(event.target.value)}
+          onClearClick={() => setValue5('')}
+        />
+      </div>
+      <div style={{ padding: 4 }}>
+        <Textinput
+          hasClear
+          label="Label"
+          size="m"
+          view="material"
+          value={value6}
+          onChange={(event) => setValue6(event.target.value)}
+          onClearClick={() => setValue6('')}
+          state="error"
+          hint="Error message"
+        />
       </div>
     </>
   )
