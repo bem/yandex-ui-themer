b4bac2aadbb5b85ff5de53ae64ec88fafc169502
Nikita Nigmatullin <nnigmat@gmail.com>
2021-05-27 12:07:46 +0300

    chore: refactor yamlText

--- src/components/Sandbox/SandboxExample/model.ts
+++ src/components/Sandbox/SandboxExample/model.ts
@@ -1,15 +1,16 @@
-import { combine } from 'effector'
+import { combine, Store } from 'effector'
 import deepmerge from 'deepmerge'
 import YAML from 'yaml'
 
 import { $designTokens } from '../../../model/tokens'
 import { $theme } from '../../../model/themes'
-import { $tokenMappings } from '../../../model/mappings'
+import { $tokenMappings, $invertedTokenMappings } from '../../../model/mappings'
 import { $cssVariables } from '../../../model/cssVariables'
 import { toDeepToken } from '../../../utils/toDeepToken'
 import { transformMappings } from '../../../utils/transformers'
 import { extractParams } from '../../../utils/resolveTokens'
-import { ParamsType } from '../../../types'
+import { DesignTokensType, ParamsType } from '../../../types'
+import { PARAM_DOT_RE } from '../../../utils/constants'
 
 export const $cssText = $cssVariables.map((variables) => {
   const cssText = Object.keys(variables).reduce(
@@ -19,41 +20,50 @@ export const $cssText = $cssVariables.map((variables) => {
   return `:root {\n${cssText}}`
 })
 
+const $dotSepDesignTokens: Store<DesignTokensType> = combine(
+  $designTokens,
+  $tokenMappings,
+  (designTokens, mappings) =>
+    Object.entries(designTokens).reduce(
+      (acc, [name, { value, ...token }]) => ({
+        ...acc,
+        [name]: {
+          ...token,
+          value: transformMappings(value, mappings),
+        },
+      }),
+      {},
+    ),
+)
+
 export const $yamlText = combine(
-  { designTokens: $designTokens, mappings: $tokenMappings, theme: $theme },
+  { designTokens: $dotSepDesignTokens, mappings: $invertedTokenMappings, theme: $theme },
   ({ designTokens, mappings, theme: { allTokens } }) => {
-    const params: ParamsType[] = []
-    const seen = new Set<string>()
-    const addedTokens = new Set<string>()
+    // Make object for yaml from designTokens
+    const yml = Object.values(designTokens).reduce((acc, { changed, path, value, rawValue }) => {
+      if (rawValue) {
+        acc.push(toDeepToken(path, { value: rawValue }))
+      } else if (changed) {
+        acc.push(toDeepToken(path, { value }))
+      }
 
-    const yml = Object.entries(designTokens).reduce(
-      (acc, [name, { changed, path, value, rawValue }]: any) => {
-        addedTokens.add(name)
+      return acc
+    }, [] as any)
 
-        if (rawValue) {
-          acc.push(toDeepToken(path, { value: rawValue }))
-          return acc
-        }
-
-        if (changed) {
-          const mappedValue = transformMappings(value.toString(), mappings)
-          acc.push(toDeepToken(path, { value: mappedValue }))
-
-          const paramsToAdd = (extractParams(value) || []).filter(({ token }) => {
-            const hasSeen = seen.has(token)
-            seen.add(token)
-            return !hasSeen
-          })
-          params.push(...paramsToAdd)
-        }
-        return acc
-      },
-      [] as any,
+    // Get all params from designTokens values
+    const params = Object.values(designTokens).reduce<ParamsType[]>(
+      (acc, { value, rawValue }) => [
+        ...acc,
+        ...(extractParams(rawValue || value, PARAM_DOT_RE) || []),
+      ],
+      [],
     )
 
+    // If param is in the theme and it was not redefined then add theme's value to the yaml
     params
-      .filter(({ token }) => !addedTokens.has(token))
-      .forEach(({ token }) => {
+      .map(({ token }) => mappings[token] || token)
+      .filter((token) => !(token in designTokens))
+      .forEach((token) => {
         if (allTokens[token]) {
           const { path, value } = allTokens[token]
           yml.push(toDeepToken(path, { value }))
