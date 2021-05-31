be623eaea3828c4242c2ef7a0ea69c3f00a8ee72
Nikita Nigmatullin <nnigmat@gmail.com>
2021-05-31 20:15:20 +0300

    feat: cypress testing environment and cypress tests

---
+++ cypress/e2e/header_spec.js
@@ -0,0 +1,55 @@
+/* globals cy */
+import { SHARED_THEME_PATH, TOKENS, CSS_TOKENS } from '../constants'
+
+describe('Theme change', () => {
+  beforeEach(() => cy.visit('/'))
+
+  it('Does change theme on click', () => {
+    cy.get('.Showcase').should('have.css', 'background-color', 'rgb(255, 255, 255)')
+
+    cy.findByText('default').click({ force: true })
+    cy.findByText('inverse').click()
+
+    cy.get('.Showcase').should('have.css', 'background-color', 'rgb(33, 31, 31)')
+  })
+})
+
+describe('Share', () => { 
+  beforeEach(() => cy.visit('/'))
+
+  it('Should be disabled when data is not set', () => {
+    cy.findByTestId('share-button').should('be.disabled')
+  })
+
+  it('Should be enabled when data is set', () => {
+    // Load theme
+    cy.findByText('Загрузить тему').click()
+    cy.findByTestId('tokens-upload-button').click()
+
+    cy.findByTestId('share-button').should('be.enabled')
+  })
+
+  it('Should change query parameter when successfully shared', () => {
+    // Load theme
+    cy.findByText('Загрузить тему').click()
+    cy.findByTestId('tokens-upload-button').click()
+
+    // Share theme
+    cy.findByTestId('share-button').click()
+
+    // Query parameter changed
+    cy.location().should((loc) => {
+      expect(loc.search).to.contain('?tokens=')
+    })
+  })
+
+  it('Should load theme', () => {
+    cy.visit(SHARED_THEME_PATH)
+
+    cy.findByText('YML').click()
+    cy.get('code').should('have.text', TOKENS)
+
+    cy.findByText('CSS').click()
+    cy.get('code').should('have.text', CSS_TOKENS)
+  })
+})
---
+++ cypress/e2e/sandbox_spec.js
@@ -0,0 +1,21 @@
+/* globals cy */
+import { TOKENS, CSS_TOKENS } from '../constants'
+
+describe('Sandbox', () => {
+  beforeEach(() => cy.visit('/'))
+
+  it('should show valid yaml and css', () => {
+
+    cy.findByText('Загрузить тему').click()
+
+    cy.findByTestId('tokens-textarea').clear().type(TOKENS)
+
+    cy.findByTestId('tokens-upload-button').click({ force: true })
+
+    cy.findByText('YML').click()
+    cy.get('code').should('have.text', TOKENS)
+
+    cy.findByText('CSS').click()
+    cy.get('code').should('have.text', CSS_TOKENS)
+  })
+})
