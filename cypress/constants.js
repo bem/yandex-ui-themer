c1756a9d88e26a639c3d6fe0b30776f3a20d1d0a
Nikita Nigmatullin <nnigmat@gmail.com>
2021-05-31 20:15:20 +0300

    feat: cypress testing environment and cypress tests

---
+++ cypress/constants.js
@@ -0,0 +1,24 @@
+export const SHARED_THEME_PATH = '/?tokens=0ed886fab82e7e9d92d21ba31fa76f48'
+
+export const TOKENS = `button:
+  viewAction:
+    fillColor:
+      base:
+        value: "#C728B3"
+      progress:
+        base:
+          value: "#C728B3"
+        process:
+          value: "#de1258"
+  viewDefault:
+    fillColor:
+      base:
+        value: "#ecb6ea"
+`
+
+export const CSS_TOKENS = `:root {
+  --button-view-action-fill-color-base: #C728B3;
+  --button-view-action-fill-color-progress-base: #C728B3;
+  --button-view-action-fill-color-progress-process: #de1258;
+  --button-view-default-fill-color-base: #ecb6ea;
+}`
