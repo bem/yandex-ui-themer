d4ec7f68226218e424d222940fe8229b6c51ef0d
Nikita Nigmatullin <nnigmat@gmail.com>
2021-06-30 17:08:26 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/yandex-ui-themer/precache-manifest.6a11b1f376c7984d54e7aab2fe35e526.js"
+  "/yandex-ui-themer/precache-manifest.29c8845bea4423d608ee32d61351b046.js"
 );
 
 self.addEventListener('message', (event) => {
