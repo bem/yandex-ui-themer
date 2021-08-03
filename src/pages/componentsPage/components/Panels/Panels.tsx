49c8b5a000085d09b6990dd426f48993fd6577ae
Nikita Nigmatullin <nnigmat@gmail.com>
2021-08-03 21:34:24 +0300

    feat: sections in Panels

--- src/pages/componentsPage/components/Panels/Panels.tsx
+++ src/pages/componentsPage/components/Panels/Panels.tsx
@@ -1,9 +1,11 @@
 import React, { FC, useState } from 'react'
-import { Input, Icon } from 'react-figma-components'
+import { Input, Icon, Disclosure } from 'react-figma-components'
 import { cn } from '@bem-react/classname'
 
 import { Panel } from '../../../../components/Panel'
 import { Divider } from '../../../../lib/lego/Divider'
+
+import { panelsTree } from './constants'
 
 import './Panels.css'
 
@@ -16,8 +18,19 @@ export type PanelsProps = {
   onPanelSelect: (panel: string) => void
 }
 
+// By default all disclosures are open
+const defaultSections = Array(Object.keys(panelsTree).length).fill(true)
+
+const includes = (component: string, search: string) =>
+  component.toLowerCase().includes(search.toLowerCase())
+
 export const Panels: FC<PanelsProps> = ({ className, panels, activePanel, onPanelSelect }) => {
   const [search, setSearch] = useState('')
+  const [sections, setSections] = useState(defaultSections)
+
+  const handleSectionChange = (index: number) => {
+    setSections((prev) => prev.map((el, i) => (i === index ? !el : el)))
+  }
 
   return (
     <div className={cnPanels(null, [className])}>
@@ -30,13 +43,43 @@ export const Panels: FC<PanelsProps> = ({ className, panels, activePanel, onPane
       />
       <Divider />
       <div className={cnPanels('Content')}>
-        {panels
-          .filter((panel) => panel.includes(search.toLocaleLowerCase()))
-          .map((panel) => (
-            <Panel key={panel} active={activePanel === panel} onClick={() => onPanelSelect(panel)}>
-              {panel}
-            </Panel>
-          ))}
+        {/* Overview is not belong to any class */}
+        {includes('overview', search) && (
+          <Panel
+            key="overview"
+            active={activePanel === 'overview'}
+            onClick={() => onPanelSelect('overview')}
+          >
+            overview
+          </Panel>
+        )}
+
+        {Object.entries(panelsTree).map(([section, components], index) => {
+          if (!components.some((component) => includes(component, search))) {
+            return <></>
+          }
+
+          return (
+            <Disclosure
+              label={section}
+              checked={sections[index]}
+              onChange={() => handleSectionChange(index)}
+              className="Panels-Disclosure"
+            >
+              {components
+                .filter((component) => includes(component, search))
+                .map((component) => (
+                  <Panel
+                    key={component}
+                    active={activePanel === component}
+                    onClick={() => onPanelSelect(component)}
+                  >
+                    {component}
+                  </Panel>
+                ))}
+            </Disclosure>
+          )
+        })}
       </div>
     </div>
   )
---
+++ src/pages/componentsPage/components/Panels/constants.ts
@@ -0,0 +1,20 @@
+export const panelsTree: Record<string, string[]> = {
+  content: ['badge', 'divider', 'icon', 'image', 'header', 'text', 'userpic'],
+  controls: [
+    'attach',
+    'button',
+    'buttonGroup',
+    'checkbox',
+    'menu',
+    'radioButton',
+    'radioBox',
+    'textarea',
+    'textinput',
+    'tumbler',
+    'select',
+  ],
+  layout: ['listTile', 'spacer'],
+  navigation: ['link', 'tabsMenu'],
+  progress: ['progress', 'spin'],
+  surface: ['drawer', 'messageBox', 'modal', 'popup', 'tabsPanes', 'tooltip'],
+}
